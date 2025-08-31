c3d237be2975649f2c551ae15ee2d011cdc97358
s4r0sh <sarosh2726s@gmail.com>
2025-08-31 18:45:20 +0500

    Fix base config for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/scholars-haven/", // <-- add this line
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    // If deploying to GitHub Pages, set base to repo name
+    // Otherwise (like Netlify), keep root "/"
+    base: mode === "gh-pages" ? "/scholars-haven/" : "/",
+    plugins: [react()],
+  };
 });
