45d144d828a62fa76382cd64703f32e3aa71bbeb
s4r0sh <sarosh2726s@gmail.com>
2026-09-15 17:42:57 +0500

    Add Subjects to nav menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,6 +27,7 @@ export default function Navbar() {
     };
 
   const navLinks = [
+    { label: "Subjects", id: "subjects" },
     { label: "Teachers", id: "teachers" },
     { label: "Services", id: "services" },
     { label: "Contact", id: "contact" },
