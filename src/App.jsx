b608ad354d2b1c1f7b24890862c4a0724a45ffcb
s4r0sh <sarosh2726s@gmail.com>
2026-09-17 12:53:45 +0500

    Add floating WhatsApp button globally

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Services from "./components/Services";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import WhatsAppButton from "./components/WhatsAppButton";
 import { Box } from "@mui/material";
 
 export default function App() {
@@ -36,6 +37,8 @@ export default function App() {
       <Box id="footer">
         <Footer />
       </Box>
+
+      <WhatsAppButton />
     </Box>
   );
 }
